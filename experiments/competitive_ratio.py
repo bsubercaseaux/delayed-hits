7ef7f00a39dbc953ed551d91d3af392d0d8c293b
Bernardo Subercaseaux <bernardosubercaseaux@gmail.com>
2024-04-04 23:54:42 -0400

    new experiment

--- experiments/competitive_ratio.py
+++ experiments/competitive_ratio.py
@@ -1,10 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from importmonkey import add_path
+
+import scipy.stats
+
+def mean_confidence_interval(data, confidence=0.99):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    min_val = np.min(a)
+    max_val = np.max(a)
+    return m, min_val, max_val
+    # return m, m-h, m+h
+
 add_path("../online")
 add_path("../offline")
 add_path("../benchmarks")
 import uniform_generator as ug
-import lru 
+import lru
 import fifo
 import belady
 
@@ -12,9 +26,9 @@ import belady
 def uniform_CR_suite(N_tests, N_pages, k, sequence_length):
     uniform_gen = ug.UniformGenerator(N_pages, seed=42)
     DATA = {}
-    
+
     for i in range(N_tests):
-        
+
         DATA[i] = {}
         algos = []
         for Z in [1] + list(range(5, 90, 5)):
@@ -28,16 +42,73 @@ def uniform_CR_suite(N_tests, N_pages, k, sequence_length):
             cost_algo = algo.run(request_sequence)
             print(f"i = {i}, {algo.get_name()} cost = {cost_algo}")
             DATA[i][algo.get_name()] = cost_algo
-    
+
     fig, ax = plt.subplots()
     for algo in algos:
-        ax.plot([i for i in range(N_tests)], [DATA[i][algo.get_name()] for i in range(N_tests)], label=algo.get_name())
-        ax.set(xlabel="Test number", ylabel="Penalty")
+        ax.plot(
+            [i for i in range(N_tests)],
+            [DATA[i][algo.get_name()] for i in range(N_tests)],
+            label=algo.get_name(),
+        )
+    ax.set(xlabel="Test number", ylabel="Penalty")
     ax.legend()
     plt.show()
 
-N_tests = 50
-N_pages = 10
-k = 9
-sequence_length = 2000
-uniform_CR_suite(N_tests, N_pages, k, sequence_length)
+
+def K_suite(N_tests, N_pages, k, sequence_length):
+    uniform_gen = ug.UniformGenerator(N_pages, seed=42)
+    DATA = {}
+
+    Z_MAX = 800
+    for i in range(N_tests):
+
+        DATA[i] = {}
+        algos = []
+        for Z in [1] + list(range(5, Z_MAX, 5)):
+            algos.append(lru.LRU(k, Z=Z))
+            # algos.append(fifo.FIFO(k, Z=Z))
+        request_sequence = uniform_gen.generate(sequence_length)
+        # print(f"Request sequence = {request_sequence}")
+        # optimal = belady.Belady(k, request_sequence)
+        # cost_optimal = optimal.run()
+        for algo in algos:
+            cost_algo, misses_algo = algo.run(request_sequence)
+            print(f"i = {i}, {algo.get_name()} cost = {cost_algo}, misses = {misses_algo}")
+            DATA[i][algo.get_name()] = (cost_algo, misses_algo)
+
+    fig, ax = plt.subplots()
+    X = []
+    Y = []
+    Z_vals = [1] + list(range(5, Z_MAX, 5))
+
+    lower = []
+    upper = []
+    for idx, algo in enumerate(algos):
+        Z = Z_vals[idx]
+        X.append(Z)
+        data_algo = [DATA[i][algo.get_name()] for i in range(N_tests)]
+        m, lm, hm = mean_confidence_interval([data[0]/data[1] for data in data_algo])
+        Y.append(m)
+        lower.append(lm)
+        upper.append(hm)
+    # for algo in algos:
+    #     ax.plot(
+    #         [i for i in range(N_tests)],
+    #         [DATA[i][algo.get_name()] for i in range(N_tests)],
+    #         label=algo.get_name(),
+    #     )
+    Y = np.array(Y)
+
+    ax.plot(X, Y, 'k-')
+    ax.plot(X, X, 'r--')    
+    ax.set(xlabel="Z", ylabel="Avg penalty per miss")
+    plt.fill_between(X,lower, upper)
+    # ax.legend()
+    plt.show()
+
+
+N_tests = 30
+N_pages = 100
+k = 10
+sequence_length = 1600
+K_suite(N_tests, N_pages, k, sequence_length)
--- online/online_api.py
+++ online/online_api.py
@@ -5,11 +5,13 @@ class OnlineCaching:
         self.k = k
         self.penalty = 0
         self.Z = Z
+        self.Q = [None]*Z
         self.in_transit = {}
         self.pending_requests = {}
         self.arrival_times = {}
         self.page_by_arrival_time = {}
         self.tick = 0
+        self.misses = 0
         self.name = "Abstract Online Caching Algorithm"
         self.optional_policy = optional_policy
         self.previous_requests = []
@@ -25,40 +27,32 @@ class OnlineCaching:
         # implement the non-optional eviction policy
         pass
     
-    def on_not_present(self, page):
-        if page not in self.pending_requests:
-            self.pending_requests[page] = []
-        self.pending_requests[page].append(self.tick)
-        if page not in self.arrival_times:
-            self.arrival_times[page] = self.tick + self.Z
-            self.page_by_arrival_time[self.tick + self.Z] = page
+    # def on_not_present(self, page):
+    #     if page not in self.pending_requests:
+    #         self.pending_requests[page] = []
+    #     self.pending_requests[page].append(self.tick)
+    #     if page not in self.arrival_times:
+    #         self.arrival_times[page] = self.tick + self.Z
+    #         self.page_by_arrival_time[self.tick + self.Z] = page
         
     def cache_page(self, page):
         self.cache.add(page)
     
     def on_request(self, page):
         self.previous_requests.append(page)
-        if page not in self.cache:
-            self.on_not_present(page)
-            
-        # from pending to cache
-        if self.tick in self.page_by_arrival_time:
-            p = self.page_by_arrival_time[self.tick]
-            for occ in self.pending_requests[p]:
-                self.penalty += self.tick - occ
-            self.page_by_arrival_time.pop(self.tick)
-            self.arrival_times.pop(p)
-            self.pending_requests.pop(p)
+        p = self.Q.pop(0)
+        if p is not None:
+            if len(self.cache) > self.k:
+                self.evict_non_optional(page)
             self.cache_page(p)
+        if page not in self.cache and page not in self.Q:
+            self.Q.append(page)
+        else:
+            self.Q.append(None)
+        if page in self.Q:
+            self.misses += 1
+            self.penalty += self.Q.index(page)
         
-        # eviction
-        if len(self.cache) > self.k: 
-                # a page needs to be evicted
-                if self.optional_policy:
-                    self.evict_optional(page)
-                else:
-                    self.evict_non_optional(page)
-                    
     def run(self, request_sequence, debug=False):
         for i, page in enumerate(request_sequence):
             if debug:
@@ -67,4 +61,4 @@ class OnlineCaching:
             if debug:
                 print(f"i={i}, cache = {self.cache}, pending = {self.pending_requests} penalty = {self.penalty}")
             self.tick += 1
-        return self.penalty
+        return self.penalty, self.misses
